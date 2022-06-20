92266cddcbbd48c73eebadd35d4d9d82fab90196
Tony Blyler <tony.blyler@cbinsights.com>
2022-06-20 13:19:55 -0400

    Update camera to support arguments to prefill/select a selection

--- chezmoi/dot_zshrc.d/executable_camera.zsh
+++ chezmoi/dot_zshrc.d/executable_camera.zsh
@@ -5,7 +5,7 @@ if command -v mpv &> /dev/null && command -v fzf &> /dev/null; then
 				set -euo pipefail
 
 				printf "%s\0" /dev/video* |
-					fzf --print0 --read0 -m -1 --height="$((LINES/4))" |
+					fzf --print0 --read0 --query "${1:-}" -m -1 --height="$((LINES/4))" |
 					xargs -0 -P 0 -I {} mpv \
 						-vf=hflip \
 						--demuxer-lavf-format=video4linux2 \
