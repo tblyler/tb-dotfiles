3c33d3d1f0413263e6af807cb262ad4487f7b086
Tony Blyler <tony@citizenmint.com>
2022-08-22 21:37:55 -0400

    Fix phpdoc treesitter being broken on MacOS arm64 and add poetry path to PATH

--- chezmoi/private_dot_config/nvim/lua/plugins/config/treesitter.lua
+++ chezmoi/private_dot_config/nvim/lua/plugins/config/treesitter.lua
@@ -1,14 +1,28 @@
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    ignore_install = {}, -- List of parsers to ignore installing
+    ignore_install = {"phpdoc", "tree-sitter-phpdoc"}, -- List of parsers to ignore installing
+    autopairs = {
+        enable = true,
+    },
     highlight = {
         enable = true,              -- false will disable the whole extension
-        disable = {},  -- list of language that will be disabled
+        disable = {"phpdoc", "tree-sitter-phpdoc"},  -- list of language that will be disabled
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
+    },
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+    },
+    indent = {
+        enable = true,
+    },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = true,
     },
 }
