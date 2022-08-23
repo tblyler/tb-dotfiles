70afacb60a723cd548cbbbd539f3b1c321489730
Tony Blyler <tony@citizenmint.com>
2022-08-22 21:44:56 -0400

    Fix autojump for MacOS

--- chezmoi/dot_zshrc.d/executable_autojump.zsh
+++ chezmoi/dot_zshrc.d/executable_autojump.zsh
@@ -1,3 +1,7 @@
 if [ -r /usr/share/autojump/autojump.sh ]; then
 	source /usr/share/autojump/autojump.sh
 fi
+
+if [ -r "${HOMEBREW_PREFIX}/share/autojump/autojump.zsh" ]; then
+	source "${HOMEBREW_PREFIX}/share/autojump/autojump.zsh"
+fi
