e01aec876e73166bff0651ede04b51ec5d54caba
Tony Blyler <tony@citizenmint.com>
2023-01-08 22:50:00 -0500

    Add wezterm config

---
+++ chezmoi/private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm';
+
+return {
+    color_scheme = 'Gruvbox Dark',
+    font = wezterm.font('SauceCodePro Nerd Font Mono', {weight='Medium'}),
+    font_size = 12.0,
+    hide_tab_bar_if_only_one_tab = true,
+    scrollback_lines = 1000,
+    enable_scroll_bar = false,
+    window_background_opacity = 0.97,
+}
