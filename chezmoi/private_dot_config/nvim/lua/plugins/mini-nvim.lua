b48ec3f923cf1b982fa196e024874d299f844ada
Tony Blyler <tony.blyler@cbinsights.com>
2023-06-28 16:19:33 -0400

    Fix blank line indentation for neovim -- just use mini-nvim's plugin:

--- chezmoi/private_dot_config/nvim/lua/plugins/mini-nvim.lua
+++ chezmoi/private_dot_config/nvim/lua/plugins/mini-nvim.lua
@@ -25,11 +25,18 @@ return {
 			require("mini.completion").setup()
 			require("mini.cursorword").setup()
 			require("mini.indentscope").setup({
+				options = {
+					border = "both",
+					indent_at_cursor = true,
+					try_as_border = true,
+				},
 				symbol = "│"
 			})
 			require("mini.surround").setup()
 			require("mini.trailspace").setup()
 
+			vim.opt.list = true
+			vim.opt.listchars = "tab:│ "
 			vim.api.nvim_command(":command Bd lua MiniBufremove.delete()")
 			vim.api.nvim_command(":command FixWhitespace lua MiniTrailspace.trim()")
 		end,
