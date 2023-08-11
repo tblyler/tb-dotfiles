712b295e890b193154b5248802cd2eefadf9f0f7
Tony Blyler <tony@citizenmint.com>
2023-08-10 23:38:15 -0400

    Add some neovim "Trouble" keybindings

--- chezmoi/private_dot_config/nvim/lua/plugins/trouble.lua
+++ chezmoi/private_dot_config/nvim/lua/plugins/trouble.lua
@@ -6,5 +6,9 @@ return {
 
 		local keys = require("helpers.keys")
 		keys.map("", "<leader>xx", "<cmd>TroubleToggle<cr>")
+		keys.map("", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>")
+		keys.map("", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>")
+		keys.map("", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>")
+		keys.map("", "<leader>xl", "<cmd>TroubleToggle loclist<cr>")
 	end
 }
