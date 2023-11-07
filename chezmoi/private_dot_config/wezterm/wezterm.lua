92be937236fc58091fe2e73cb8d0f3fbaaa7e667
Tony Blyler <tony.blyler@cbinsights.com>
2023-11-07 11:50:12 -0500

    Use GoMono for WezTerm

--- chezmoi/private_dot_config/wezterm/wezterm.lua
+++ chezmoi/private_dot_config/wezterm/wezterm.lua
@@ -3,7 +3,8 @@ local wezterm = require 'wezterm';
 return {
     --color_scheme = 'Gruvbox dark, hard (base16)',
     color_scheme = 'Dracula Pro (Morbius)',
-    font = wezterm.font('SauceCodePro Nerd Font Mono', {weight='Medium'}),
+    --font = wezterm.font('SauceCodePro Nerd Font Mono', {weight='Medium'}),
+    font = wezterm.font('GoMono Nerd Font', {weight='Medium'}),
     font_size = 12.0,
     hide_tab_bar_if_only_one_tab = true,
     scrollback_lines = 1000,
