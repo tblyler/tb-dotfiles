b24063125b40c93b43ebdfd2e99a096218b8ef75
Tony Blyler <git@tonyblyler.com>
2024-05-06 14:26:36 -0400

    Add asdf to .zshenv to fix GOPATH issues on a purely asdf Go runtime install

--- chezmoi/dot_zshrc.d/executable_asdf.zsh
+++ chezmoi/dot_zshrc.d/executable_asdf.zsh
@@ -1,11 +1,3 @@
-if [ -r ~/.asdf/asdf.sh ]; then
-	ASDF_FORCE_PREPEND=1 . ~/.asdf/asdf.sh
-fi
-
-if command -v brew &> /dev/null && [ -r "$(brew --prefix)/opt/asdf/libexec/asdf.sh" ]; then
-	. "$(brew --prefix)/opt/asdf/libexec/asdf.sh"
-fi
-
 if [ -n "$ASDF_DIR" ]; then
 	# append completions to fpath
 	fpath=(${ASDF_DIR}/completions $fpath)
