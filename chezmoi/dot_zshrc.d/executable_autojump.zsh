39ae69891aa5666a3e0f36d11e02b32a1f15ee15
Tony Blyler <git@tonyblyler.com>
2024-10-29 09:04:48 -0400

    fix autojump sourcing on arch

--- chezmoi/dot_zshrc.d/executable_autojump.zsh
+++ chezmoi/dot_zshrc.d/executable_autojump.zsh
@@ -1,7 +1,7 @@
-if [ -r /usr/share/autojump/autojump.sh ]; then
+if [ -r /usr/share/autojump/autojump.zsh ]; then
+	source /usr/share/autojump/autojump.zsh
+elif [ -r /usr/share/autojump/autojump.sh ]; then
 	source /usr/share/autojump/autojump.sh
-fi
-
-if [ -r "${HOMEBREW_PREFIX}/share/autojump/autojump.zsh" ]; then
+elif [ -r "${HOMEBREW_PREFIX}/share/autojump/autojump.zsh" ]; then
 	source "${HOMEBREW_PREFIX}/share/autojump/autojump.zsh"
 fi
