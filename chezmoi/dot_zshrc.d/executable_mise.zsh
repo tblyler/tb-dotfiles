ca766967c1b2c54c06a702c984524103e7c26a1e
Tony Blyler <git@tonyblyler.com>
2026-06-18 10:04:04 -0400

    Add mise completions to zsh

--- chezmoi/dot_zshrc.d/executable_mise.zsh
+++ chezmoi/dot_zshrc.d/executable_mise.zsh
@@ -1,3 +1,21 @@
+#!/usr/bin/zsh
 if command -v mise &> /dev/null; then
 	eval "$(mise activate zsh)"
+
+	(
+		set -euo pipefail
+		readonly MISE_COMPLETION_FILE="${ZSH_COMPLETIONS_DIR}/_mise"
+
+		if [ -r "$MISE_COMPLETION_FILE" ]; then
+			zstat -H COMPLETION_FILE_STAT "$MISE_COMPLETION_FILE"
+			zstat -H MISE_FILE_STAT "$(whence -p mise)"
+
+			if [ "${COMPLETION_FILE_STAT[ctime]}" -ge "${MISE_FILE_STAT[ctime]}" ]; then
+				exit 0
+			fi
+		fi
+
+		mise completion zsh > "$MISE_COMPLETION_FILE"
+		chmod +x "$MISE_COMPLETION_FILE"
+	)
 fi
